8ee453feefa9fbdc70cf78e96ac1a4c69ecfe320
Geir Agustsson <geiragustsson@localhost.localdomain>
2018-10-28 21:27:04 +0100

    Scrape test first win

--- scraper.py
+++ scraper.py
@@ -10,9 +10,11 @@ soup = BeautifulSoup(content, 'html.parser')
 soup_dic = {}
 soup_dic["parser"] = soup.declared_html_encoding
 soup_dic["prettified"] = soup.prettify()
-soup_dic["article"] = soup.find(id="content")
+article = soup.find("section", attrs={"role":"main"})
+soup_dic["article"] = article
+soup_dic["article_text"] = article.text.strip()
 
-print(soup_dic["article"])
+print(soup_dic["article_text"])
 
 #print(soup.title.string)
 #print(soup.p)
